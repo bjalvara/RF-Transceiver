62353434dd28224ae036f19f46e011a93cfd9f41
bjalvara <46462045+bjalvara@users.noreply.github.com>
2019-05-22 16:25:24 -0700

    Update ReciveRF.py

--- RF-python-script/ReciveRF.py
+++ RF-python-script/ReciveRF.py
@@ -2,24 +2,23 @@ from datetime import datetime
 import matplotlib.pyplot as pyplot
 import RPi.GPIO as GPIO
 
-RECEIVED_SIGNAL = [[], []] #[[time of reading], [signal reading]]
+RECEIVED_SIGNAL = [[], []]  #[[time of reading], [signal reading]]
 MAX_DURATION = 5
-RECEIVE_PIN = 4
+RECEIVE_PIN = 23
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RECEIVE_PIN, GPIO.IN)
     cumulative_time = 0
     beginning_time = datetime.now()
-    print('**Started Recording')
+    print('**Started recording**')
     while cumulative_time < MAX_DURATION:
         time_delta = datetime.now() - beginning_time
         RECEIVED_SIGNAL[0].append(time_delta)
         RECEIVED_SIGNAL[1].append(GPIO.input(RECEIVE_PIN))
-        cumulative_time = time_delta.second
-    
+        cumulative_time = time_delta.seconds
     print('**Ended recording**')
-    print(len(RECEIVED_SIGNAL[0]), 'sample recorded')
+    print(len(RECEIVED_SIGNAL[0]), 'samples recorded')
     GPIO.cleanup()
 
     print('**Processing results**')
