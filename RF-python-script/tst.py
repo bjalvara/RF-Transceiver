51697b04e9555a2c4cda8e9048944604d82e1a7a
bjalvara <46462045+bjalvara@users.noreply.github.com>
2019-05-28 15:26:32 -0700

    Update tst.py

--- RF-python-script/tst.py
+++ RF-python-script/tst.py
@@ -1,19 +1,24 @@
 import RPi.GPIO as GPIO
+from gpiozero import MCP3008
 from time import sleep
+
 GPIO.setmode(GPIO.BCM)
 
 RECEIVE_PIN = 27
 
 GPIO.setup(RECEIVE_PIN,GPIO.IN)
 
+rec = MCP3008(0)
+while True:
 
-try:
-    while True:
-        val = GPIO.input(RECEIVE_PIN)
-        if GPIO.input(RECEIVE_PIN):
-            print(val)
-        else:
-            print(val)
+    print(rec.value)
+#try:
+ #   while True:
+  #      
+   #     if GPIO.input(RECEIVE_PIN):
+    #        print(val)
+     #   else:
+      #      print(val)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+#except KeyboardInterrupt:
+ #   GPIO.cleanup()
